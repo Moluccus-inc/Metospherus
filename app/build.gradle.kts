a0d3a4c1bef17e02008b2119751acf753e425537
la niina <la.niina.me@gmail.com>
2023-08-28 03:04:02 +0300

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,13 +94,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-/**
-tasks.register("generateVersionTxt") {
-    doLast {
-        android.defaultConfig.versionName?.let { file("./version.txt").writeText(it) }
-    }
-}
-**/
 
 tasks.register("assembleRelease2") {
     group = "assemble"
