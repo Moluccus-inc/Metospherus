9100b30b03e63434437d04a8e85948ef68b27dd4
la niina <118225191+la-niina@users.noreply.github.com>
2023-09-01 10:47:27 -0700

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         //noinspection EditedTargetSdkVersion
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.9"
+        versionName = "1.1.0"
 
         vectorDrawables {
             useSupportLibrary = true
