39e260435910cee35d3f06f7e38f7fe7ee4dbab6
la niina <la.niina.me@gmail.com>
2023-09-03 23:31:40 +0300

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,7 @@ android {
         }
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.2"
+        kotlinCompilerExtensionVersion = "1.4.4"
     }
     packagingOptions {
         resources {
